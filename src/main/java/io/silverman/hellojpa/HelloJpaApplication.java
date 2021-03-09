6a3a3472372e4c28134ad2a4fd59d6c253e5a1a8
silvermanseoul <silverman0822@gmail.com>
2021-03-10 00:05:09 +0900

    Initial commit

---
+++ src/main/java/io/silverman/hellojpa/HelloJpaApplication.java
@@ -0,0 +1,13 @@
+package io.silverman.hellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/silverman/hellojpa/HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.silverman.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
