0c19c332430aa036db9c957a77cdb21a75c4bb1a
silvermanseoul <silverman0822@gmail.com>
2021-03-12 15:13:52 +0900

    Implement MemberService

---
+++ src/main/java/io/silverman/hellojpa/service/MemberService.java
@@ -0,0 +1,40 @@
+package io.silverman.hellojpa.service;
+
+import io.silverman.hellojpa.domain.Member;
+import io.silverman.hellojpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> foundMembers = memberRepository.findByName(member.getName());
+        if (!foundMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+}
