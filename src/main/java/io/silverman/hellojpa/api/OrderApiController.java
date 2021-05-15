6bcbdbe459b4b5b0351c2ee06d8fb16354f6ba19
silvermanseoul <silverman0822@gmail.com>
2021-05-15 11:00:35 +0900

    Implement OrderApiController

---
+++ src/main/java/io/silverman/hellojpa/api/OrderApiController.java
@@ -0,0 +1,67 @@
+package io.silverman.hellojpa.api;
+
+import io.silverman.hellojpa.domain.*;
+import io.silverman.hellojpa.repository.OrderRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    //== 엔티티를 조회해서 DTO로 변환 ==//
+
+    @GetMapping("/api/v2.0/orders")
+    public ResponseWrapper<List<OrderDto>> ordersV20() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(collectingAndThen(toList(), ResponseWrapper::new));
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String memberName;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            memberName = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+}
